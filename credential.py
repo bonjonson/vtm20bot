f90212fc6e2f5bf9dfb29c7ea5a3bb446d49bf29
Artem Grishin <artem.grishin@sbermegamarket.ru>
2023-03-07 23:37:42 +0300

    fix

--- credential.py
+++ credential.py
@@ -1,6 +1,8 @@
 """Импорт API ключа"""
-from telebot import AsyncTeleBot
-connectto = AsyncTeleBot(API_KEY)
+import telebot
+# from telebot import AsyncTeleBot
+connectto = telebot.AsyncTeleBot(API_KEY)
+connectto = telebot
 
 if __name__ == "__main__":
     pass
